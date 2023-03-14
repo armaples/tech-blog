168cf85080d1b69259522227395ef8b8830ee921
ALYSSA_MAPLES <raynemaples@gmail.com>
2023-03-14 19:02:33 -0400

    Added seed file

---
+++ seeds/seed.js
@@ -0,0 +1,34 @@
+const sequelize = require('../config/connection');
+const { User, Post, Comment } = require('../models');
+
+const userData = require('./userData.json');
+const postData = require('./postData.json');
+const commentData = require('./commentData.json');
+
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true });
+
+    const users = await User.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    for (const posts of postData) {
+        await Post.create({
+            ...posts,
+            userId: users[Math.floor(Math.random() * users.length)].id,
+        });
+    };
+
+    for (const comments of commentData) {
+        await Comment.create({
+            ...comments,
+            userId: users[Math.floor(Math.random() * users.length)].id,
+            postId: posts[Math.floor(Math.random() * users.length)].id,
+        })
+    };
+
+    process.exit(0);
+};
+
+seedDatabase();
