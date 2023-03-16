2208c5251046d2061467f90079431e61906f745f
ALYSSA_MAPLES <raynemaples@gmail.com>
2023-03-16 12:43:03 -0400

    Finished routes

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,11 +1,110 @@
+const router = require('express').Router();
+const { Post, Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
+
 // Create post
+router.post('/', withAuth, async (req, res) => {
+    try {
+        const newPost = await Post.create({
+            ...req.body,
+            userId: req.session.userId
+        });
+
+        res.status(200).json(newPost);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
 
 // Update post
+router.put('/:id', withAuth, async (req, res) => {
+    try {
+        const updatedPost = await Post.update(
+            req.body, {
+                where: {
+                    id: req.params.id
+                },
+            });
+        
+        res.status(200).json(updatedPost);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
 
 // Delete post
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const postData = await Post.destroy({
+            where: {
+                id: req.params.id,
+                userId: req.session.userId,
+            },
+        });
+
+        if (!postData) {
+            res.status(400).json({ message: 'No post found with this id!' });
+            return;
+        }
+
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 // Add comment
+router.post('/:id/comment', withAuth, async (req, res) => {
+    try {
+        const commentData = await Comment.create({
+            content: req.body.content
+        })
+
+        res.status(200).json(commentData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
 
 // Update comment
+router.put('/:id/comment/:commentId', withAuth, async (req, res) => {
+    try {
+        const updatedComment = await Comment.update(
+            {
+                content: req.body.content
+            },
+            {
+                where: {
+                    id: req.params.commentId
+                }
+            }
+        );
+        res.status(200).json(updatedComment)
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 // delete comment
+router.delete('/:id/comment/:commentId', withAuth, async (req, res) => {
+    try {
+        const commentData = await Comment.destroy({
+            where: {
+                id: req.params.commentId
+            }
+        });
+
+        if (!commentData) {
+            res.status(404).json({
+                message: "No comments found with that id!"
+            });
+            return;
+        }
+
+        res.status(200).json(commentData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,9 +3,78 @@ const { User, Post, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
 // GET All blog posts
+router.get('/', async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        });
+
+        const posts = postData.map((post) => post.get({ plain: true }));
+
+        res.render('homepage', {
+            posts,
+            loggedIn: req.session.loggedIn
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
 
 // GET single blog post
+router.get('/post/:id', async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        });
 
-// GET dashboard
+        const post = postData.get({ plain: true });
+
+        res.render('post', {
+            ...post,
+            loggedIn: req.session.loggedIn
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+// GET profile
+router.get('/profile', withAuth, async (req, res) => {
+    try {
+        const userData = await User.findByPk(req.session.userId, {
+            attributes: { exclude: ['password'] },
+            include: [{ model: Post }]
+        });
+
+        const user = userData.get({ plain: true });
+
+        res.render('profile', {
+            ...user,
+            loggedIn: true
+        })
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 // login
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/profile');
+        return;
+    }
+
+    res.render('login');
+})
+
+module.exports = router;
